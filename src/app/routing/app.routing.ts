0fa73f3d7a2f04ce3a24a50a32fe0a4d6e1d9a51
Thibault Dulon <t.dulon@betclicgroup.com>
2017-06-12 14:41:28 +0200

    remove hash and add htaccess for url rewriting

--- src/app/routing/app.routing.ts
+++ src/app/routing/app.routing.ts
@@ -18,4 +18,4 @@ const appRoutes: Routes = [
 	}
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true });
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
