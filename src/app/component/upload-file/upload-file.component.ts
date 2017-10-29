c67246052c092b84a192e78ad8f2989d3a3def1b
Thibault Dulon <thibault.dulon@gmail.com>
2017-10-29 15:44:51 +0100

    add tslint + fix environment

--- src/app/component/upload-file/upload-file.component.ts
+++ src/app/component/upload-file/upload-file.component.ts
@@ -22,14 +22,13 @@ export class UploadFileComponent implements OnInit, OnChanges {
 		this.serviceUrl = environment.apiUrl;
 
 		if (this.stepId != null && this.stepId > 0) {
-			var that = this;
+			let that = this;
 			this.itineraryService.getStepPictures(this.stepId).subscribe(
 				result => that.assignStepPictures(result),
 				error => alert(error)
 			);
-		}
-		else if (this.stopId != null && this.stopId > 0) {
-			var that = this;
+		} else if (this.stopId != null && this.stopId > 0) {
+			let that = this;
 			this.itineraryService.getStopPictures(this.stopId).subscribe(
 				result => that.assignStopPictures(result),
 				error => alert(error)
@@ -42,17 +41,19 @@ export class UploadFileComponent implements OnInit, OnChanges {
 	}
 
 	uploadChange($event) {
-		var files = $event.target.files;
+		let files = $event.target.files;
 
 		this.isLoading = true;
-		var that = this;
+		let that = this;
 
+		
 		if (this.stepId) {
 			this.itineraryService.uploadImages(this.stepId, files).then((result) => {
 				that.isLoading = false;
-
+				console.log(result);
+				
 				result.result.forEach(function (element) {
-					var img = new Picture({
+					let img = new Picture({
 						stepId: that.stepId,
 						url: that.serviceUrl + '/' + element.url,
 						id: element.id,
@@ -64,13 +65,12 @@ export class UploadFileComponent implements OnInit, OnChanges {
 			}, (error) => {
 				alert(error);
 			});
-		}
-		else {
+		} else {
 			this.itineraryService.uploadStopImages(this.stopId, files).then((result) => {
 				that.isLoading = false;
 
 				result.result.forEach(function (element) {
-					var img = new Picture({
+					let img = new Picture({
 						stopId: that.stopId,
 						url: that.serviceUrl + '/' + element.url,
 						id: element.id,
@@ -86,7 +86,7 @@ export class UploadFileComponent implements OnInit, OnChanges {
 	}
 
 	removePicture(imageId: number) {
-		var that = this;
+		let that = this;
 		this.itineraryService.deletePicture(imageId).subscribe(
 			result => that.successfullyRemoved(imageId),
 			error => alert(error)
@@ -98,14 +98,13 @@ export class UploadFileComponent implements OnInit, OnChanges {
 
 	ngOnChanges(e) {
 		if (this.stepId != null && this.stepId > 0) {
-			var that = this;
+			let that = this;
 			this.itineraryService.getStepPictures(this.stepId).subscribe(
 				result => that.assignStepPictures(result),
 				error => alert(error)
 			);
-		}
-		else if (this.stopId != null && this.stopId > 0) {
-			var that = this;
+		} else if (this.stopId != null && this.stopId > 0) {
+			let that = this;
 			this.itineraryService.getStopPictures(this.stopId).subscribe(
 				result => that.assignStopPictures(result),
 				error => alert(error)
@@ -117,7 +116,7 @@ export class UploadFileComponent implements OnInit, OnChanges {
 		this.images = this.images.filter(i => i.id != id);
 	}
 	private assignStepPictures(pictures: Array<Picture>) {
-		var that = this;
+		let that = this;
 		pictures.forEach(function (element) {
 			that.images.push(new Picture({
 				id: element.id,
@@ -129,18 +128,19 @@ export class UploadFileComponent implements OnInit, OnChanges {
 		});
 	}
 	private assignStopPictures(pictures: Array<Picture>) {
-		var that = this;
+		let that = this;
 		pictures.forEach(function (element) {
-			var img = new Picture({
+			let img = new Picture({
 				id: element.id,
 				url: that.serviceUrl + '/' + element.url,
 				caption: element.caption,
 				date: element.date.split('T')[0]
 			});
-			if (that.stepId)
+			if (that.stepId) {
 				img.stepId = that.stepId;
-			else
+			} else {
 				img.stopId = that.stopId;
+			}
 			that.images.push(img);
 		});
 	}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,10 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
-
 export const environment = {
 	production: false,
-	// url: 'http://localhost:4200',
-	// apiUrl: 'http://localhost:3000'
-	url: 'https://itineraris.fr',	
-	apiUrl: 'https://api.itineraris.fr'
+	url: 'http://localhost:4200',
+	apiUrl: 'http://localhost:3000'
+	// url: 'https://itineraris.fr',
+	// apiUrl: 'https://api.itineraris.fr'
 };
